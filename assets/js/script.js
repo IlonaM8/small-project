217b821d797c134e37ae4bb6808258c6ce9e6c93
IlonaM8 <edoilo.3521@gmail.com>
2021-01-07 22:55:54 +0100

    nav bar

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@
 function myFunction() {
     var x = document.getElementById('myNavbar');
     if (x.className === 'navbar') {
-        x.className += 'responsive';
+        x.className += ' responsive';
     } else {
         x.className = 'navbar';
     }
